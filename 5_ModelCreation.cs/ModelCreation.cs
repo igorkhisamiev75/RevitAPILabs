0bd7c45b1f53fcbf9f8d4916d20c12b69350b150
igorkhisamiev75 <khisamievigor@gmail.com>
2021-03-30 21:05:46 +0300

    Добавил разрезы

---
+++ 5_ModelCreation.cs/ModelCreation.cs
@@ -0,0 +1,253 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Structure;
+using Autodesk.Revit.UI;
+
+using Util;
+#endregion
+
+namespace IntroCs
+{
+    [Transaction(TransactionMode.Manual)]
+    public class ModelCreation: IExternalCommand
+    {
+        Application _app;
+        Document _doc;
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet element)
+        {
+            UIApplication rvtUIApp = commandData.Application;
+            UIDocument uiDoc=rvtUIApp.ActiveUIDocument;
+            _app = rvtUIApp.Application;
+            _doc = uiDoc.Document;
+
+            using (Transaction transaction=new Transaction(_doc))
+            {
+                transaction.Start("Create House");
+
+                //создадим простой дом
+                CreateHouse();
+                transaction.Commit();
+            }
+            return Result.Succeeded;
+        }
+
+        private void CreateHouse()
+        {
+            List<Wall> walls = CreateWalls(); //создаём стены
+
+            AddDoor(walls[0]); //добавляем дверь в первую стену
+
+            for (int i = 1; i < 4; i++)
+            {
+                AddWindow(walls[i]);
+            }
+
+            AddRoof(walls);
+
+        }
+
+        public void AddRoof(List<Wall> walls)
+        {
+            // Hard coding the roof type we will use. 
+            // E.g., "Basic Roof: Generic - 400mm" 
+
+            const string roofFamilyName = "Базовая крыша";
+            const string roofTypeName = Constant.RoofTypeName; 
+            const string roofFamilyAndTypeName = roofFamilyName + ": " + roofTypeName;
+
+            // Find the roof type 
+
+            RoofType roofType = (RoofType)ElementFiltering.FindFamilyType(_doc, typeof(RoofType), roofFamilyName, roofTypeName, null);
+
+            if (roofType == null)
+            {
+                TaskDialog.Show("Add roof","Cannot find (" + roofFamilyAndTypeName +"). Maybe you use a different template? Try with DefaultMetric.rte.");
+            }
+
+            // Wall thickness to adjust the footprint of the walls 
+            // to the outer most lines. 
+            // Note: this may not be the best way, 
+            // but we will live with this for this exercise. 
+
+            
+            double wallThickness = walls[0].Width;
+
+            double dt = wallThickness / 2.0;
+            List<XYZ> dts = new List<XYZ>(5);
+            dts.Add(new XYZ(-dt, -dt, 0.0));
+            dts.Add(new XYZ(dt, -dt, 0.0));
+            dts.Add(new XYZ(dt, dt, 0.0));
+            dts.Add(new XYZ(-dt, dt, 0.0));
+            dts.Add(dts[0]);
+
+            // Set the profile from four walls 
+
+            CurveArray footPrint = new CurveArray();
+            for (int i = 0; i <= 3; i++)
+            {
+                LocationCurve locCurve = (LocationCurve)walls[i].Location;
+                XYZ pt1 = locCurve.Curve.GetEndPoint(0) + dts[i];
+                XYZ pt2 = locCurve.Curve.GetEndPoint(1) + dts[i + 1];
+                Line line = Line.CreateBound(pt1, pt2);
+                footPrint.Append(line);
+            }
+
+            // Get the level2 from the wall 
+
+            ElementId idLevel2 = walls[0].get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).AsElementId();
+           
+            Level level2 = (Level)_doc.GetElement(idLevel2); // since 2013
+
+            // Footprint to model curve mapping 
+
+            ModelCurveArray mapping = new ModelCurveArray();
+
+            // Create a roof. 
+
+            FootPrintRoof aRoof = _doc.Create.NewFootPrintRoof(footPrint, level2, roofType, out mapping);
+
+            foreach (ModelCurve modelCurve in mapping)
+            {
+                aRoof.set_DefinesSlope(modelCurve, true);
+                aRoof.set_SlopeAngle(modelCurve, 0.5);
+            }
+        }
+
+        public void AddWindow(Wall hostWall)
+        {
+            // Hard coding the window type we will use. 
+            // E.g., "M_Fixed: 0915 x 1830mm 
+
+            const string windowFamilyName = Constant.WindowFamilyName;
+            const string windowTypeName = Constant.WindowTypeName;
+            const string windowFamilyAndTypeName = windowFamilyName + ": " + windowTypeName;
+            double sillHeight = Constant.MmToFeet(915);
+
+            // Get the door type to use. 
+
+            FamilySymbol windowType = (FamilySymbol)ElementFiltering.FindFamilyType(_doc, typeof(FamilySymbol), windowFamilyName, windowTypeName, BuiltInCategory.OST_Windows);
+            if (windowType == null)
+            {
+                TaskDialog.Show("Add window","Cannot find (" +windowFamilyAndTypeName +"). Maybe you use a different template? Try with DefaultMetric.rte.");
+            }
+
+            if (!windowType.IsActive)
+                windowType.Activate();
+
+            // Get the start and end points of the wall. 
+
+            LocationCurve locCurve = (LocationCurve)hostWall.Location;
+
+            XYZ pt1 = locCurve.Curve.GetEndPoint(0);
+            XYZ pt2 = locCurve.Curve.GetEndPoint(1);
+            // Calculate the mid point. 
+            XYZ pt = (pt1 + pt2) / 2.0;
+
+            // we want to set the reference as a bottom of the wall or level1. 
+
+            ElementId idLevel1 = hostWall.get_Parameter(BuiltInParameter.WALL_BASE_CONSTRAINT).AsElementId();
+            
+            Level level1 = (Level)_doc.GetElement(idLevel1); // since 2013
+
+            // Finally create a window. 
+
+            FamilyInstance aWindow = _doc.Create.NewFamilyInstance(pt, windowType, hostWall, level1, StructuralType.NonStructural);
+
+            aWindow.get_Parameter(BuiltInParameter.INSTANCE_SILL_HEIGHT_PARAM).Set(sillHeight);
+        }
+
+        public void AddDoor(Wall hostWall)
+        {
+            const string doorFamilyName = Constant.DoorFamilyName;
+            const string doorTypeName = Constant.DoorTypeName;
+            const string doorFamilyAndTypeName = doorFamilyName + ": " + doorTypeName;
+
+            // Get the door type to use. 
+
+            FamilySymbol doorType = (FamilySymbol)ElementFiltering.FindFamilyType(_doc, typeof(FamilySymbol), doorFamilyName, doorTypeName, BuiltInCategory.OST_Doors);
+
+            if (doorType == null)
+            {
+                TaskDialog.Show("Add door", "Cannot find (" + doorFamilyAndTypeName + "). maybe you use a differnt template? Try with DefaultMetric.rte.");
+            }
+
+            if (!doorType.IsActive)
+                doorType.Activate();
+
+            // Get the start and end points of the wall. 
+
+            LocationCurve locCurve = (LocationCurve)hostWall.Location;
+
+            XYZ pt1 = locCurve.Curve.GetEndPoint(0);
+            XYZ pt2 = locCurve.Curve.GetEndPoint(1);
+
+            XYZ pt = (pt1 + pt2) / 2.0;   // Calculate the mid point. 
+
+            ElementId idLevel1 = hostWall.get_Parameter(BuiltInParameter.WALL_BASE_CONSTRAINT).AsElementId(); //level wall
+
+            Level level1 = (Level)_doc.GetElement(idLevel1);
+
+            FamilyInstance aDoor = _doc.Create.NewFamilyInstance(pt, doorType, hostWall, level1, StructuralType.NonStructural);
+
+        }
+
+        public List<Wall> CreateWalls()
+        {
+            double width = Constant.MmToFeet(10000.0);
+            double depth = Constant.MmToFeet(5000.0);
+
+            Level level1 = (Level)ElementFiltering.FindElement(_doc, typeof(Level), "Level 1", null);
+            if (level1 == null)
+            {
+                TaskDialog.Show("Create walls", "Cannot find (Level 1). Maybe you use a different template? Try with DefaultMetric.rte");
+                return null;
+            }
+
+            Level level2 = (Level)ElementFiltering.FindElement(_doc, typeof(Level), "Level 2", null);
+            if (level2 == null)
+            {
+                TaskDialog.Show("Create walls", "Cannot find(Level 2). Maybe you use a different template? Try with DifaulMetric.rte.");
+                return null;
+            }
+
+            double dx = width / 2.0;
+            double dy = depth / 2.0;
+
+            List<XYZ> pts = new List<XYZ>(5);
+            pts.Add(new XYZ(-dx, -dy, 0.0));
+            pts.Add(new XYZ(dx, -dy, 0.0));
+            pts.Add(new XYZ(dx, dy, 0.0));
+            pts.Add(new XYZ(-dx, dy, 0.0));
+            pts.Add(pts[0]);
+
+
+            bool isStructural = false; // Flag for structural wall or not. 
+
+            List<Wall> walls = new List<Wall>(4);  // Save walls we create. 
+
+            for (int i = 0; i <= 3; i++)
+            {
+                Line baseCurve = Line.CreateBound(pts[i], pts[i + 1]); //создаем границу
+
+                Wall aWall = Wall.Create(_doc, baseCurve, level1.Id, isStructural); //create a wall
+
+                aWall.get_Parameter(BuiltInParameter.WALL_HEIGHT_TYPE).Set(level2.Id); //set the top constraction to level 2
+
+                walls.Add(aWall);
+
+            }
+
+            _doc.Regenerate();
+            _doc.AutoJoinElements();
+
+            return walls;
+
+        }
+    }
+}
---
+++ CreateSection/CreateSection.cs
@@ -0,0 +1,168 @@
+﻿#region Namespaces
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+#endregion
+
+namespace CreateWallSectionView
+{
+    [Transaction(TransactionMode.Manual)]
+    public class Command : IExternalCommand
+    {
+        Application _app;
+        Document _doc;
+        /// <summary>
+        /// Return a section box for a view perpendicular 
+        /// to the given wall location line.
+        /// </summary>
+        BoundingBoxXYZ GetSectionViewPerpendiculatToWall(Wall wall)
+        {
+            LocationCurve lc = wall.Location
+              as LocationCurve;
+
+            // Using 0.5 and "true" to specify that the 
+            // parameter is normalized places the transform
+            // origin at the center of the location curve
+
+            Transform curveTransform = lc.Curve
+              .ComputeDerivatives(0.5, true);
+
+            // The transform contains the location curve
+            // mid-point and tangent, and we can obtain
+            // its normal in the XY plane:
+
+            XYZ origin = curveTransform.Origin;
+            XYZ viewdir = curveTransform.BasisX.Normalize();
+            XYZ up = XYZ.BasisZ;
+            XYZ right = up.CrossProduct(viewdir);
+
+            // Set up view transform, assuming wall's "up" 
+            // is vertical. For a non-vertical situation 
+            // such as section through a sloped floor, the 
+            // surface normal would be needed
+
+            Transform transform = Transform.Identity;
+            transform.Origin = origin;
+            transform.BasisX = right;
+            transform.BasisY = up;
+            transform.BasisZ = viewdir;
+
+            BoundingBoxXYZ sectionBox = new BoundingBoxXYZ();
+            sectionBox.Transform = transform;
+
+            // Min & Max X values -10 and 10 define the 
+            // section line length on each side of the wall.
+            // Max Y (12) is the height of the section box.
+            // Max Z (5) is the far clip offset.
+
+            double d = wall.WallType.Width;
+            BoundingBoxXYZ bb = wall.get_BoundingBox(null);
+            double minZ = bb.Min.Z;
+            double maxZ = bb.Max.Z;
+            double h = maxZ - minZ;
+
+            sectionBox.Min = new XYZ(-2 * d, -1, 0);
+            sectionBox.Max = new XYZ(2 * d, h + 1, 5);
+
+            return sectionBox;
+        }
+
+        /// <summary>
+        /// Return a section box for a view parallel
+        /// to the given wall location line.
+        /// </summary>
+        BoundingBoxXYZ GetSectionViewParallelToWall(Wall wall)
+        {
+            LocationCurve lc = wall.Location
+              as LocationCurve;
+
+            Curve curve = lc.Curve;
+
+            // view direction sectionBox.Transform.BasisZ
+            // up direction sectionBox.Transform.BasisY
+            // right hand is computed so that (right, up, view direction) form a left handed coordinate system.  
+            // crop region projections of BoundingBoxXYZ.Min and BoundingBoxXYZ.Max onto the view cut plane
+            // far clip distance difference of the z-coordinates of BoundingBoxXYZ.Min and BoundingBoxXYZ.Max
+
+            XYZ p = curve.GetEndPoint(0);
+            XYZ q = curve.GetEndPoint(1);
+            XYZ v = q - p;
+
+            BoundingBoxXYZ bb = wall.get_BoundingBox(null);
+            double minZ = bb.Min.Z;
+            double maxZ = bb.Max.Z;
+
+            double w = v.GetLength();
+            double h = maxZ - minZ;
+            double d = wall.WallType.Width;
+            double offset = 0.1 * w;
+
+            XYZ min = new XYZ(-w, minZ - offset, -offset);
+            //XYZ max = new XYZ( w, maxZ + offset, 0 ); // section view dotted line in center of wall
+            XYZ max = new XYZ(w, maxZ + offset, offset); // section view dotted line offset from center of wall
+
+            XYZ midpoint = p + 0.5 * v;
+            XYZ walldir = v.Normalize();
+            XYZ up = XYZ.BasisZ;
+            XYZ viewdir = walldir.CrossProduct(up);
+
+            Transform t = Transform.Identity;
+            t.Origin = midpoint;
+            t.BasisX = walldir;
+            t.BasisY = up;
+            t.BasisZ = viewdir;
+
+            BoundingBoxXYZ sectionBox = new BoundingBoxXYZ();
+            sectionBox.Transform = t;
+            sectionBox.Min = min;
+            sectionBox.Max = max;
+
+            return sectionBox;
+        }
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIApplication uiApp = commandData.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            _app = uiApp.Application;
+            _doc = uiDoc.Document;
+
+            Reference reference = uiDoc.Selection.PickObject(ObjectType.Element);
+            Element element = uiDoc.Document.GetElement(reference);
+
+            Wall wall = (Wall)element;
+
+
+
+            ViewFamilyType vft = new FilteredElementCollector(_doc)
+                .OfClass(typeof(ViewFamilyType))
+                .Cast<ViewFamilyType>()
+                .FirstOrDefault<ViewFamilyType>(x =>
+                 ViewFamily.Section == x.ViewFamily);
+
+            // Determine section box
+
+            BoundingBoxXYZ sectionBox = GetSectionViewParallelToWall(wall);
+            //= GetSectionViewPerpendiculatToWall( wall );
+
+            // Create wall section view
+
+            using (Transaction tx = new Transaction(_doc))
+            {
+                tx.Start("Create Wall Section View");
+
+                ViewSection.CreateSection(_doc, vft.Id, sectionBox);
+
+                tx.Commit();
+            }
+            return Result.Succeeded;
+        }
+    }
+}
