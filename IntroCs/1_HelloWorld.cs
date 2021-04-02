7b75ae599afd9318edcbb9df756179e4f7c9d11e
igorkhisamiev75 <khisamievigor@gmail.com>
2021-04-02 15:21:30 +0300

    UI days2

---
+++ IntroCs/1_HelloWorld.cs
@@ -0,0 +1,138 @@
+﻿#region Namespaces
+using System;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.ApplicationServices; // This is for Revit Application
+#endregion
+
+#region Description
+// Revit Intro Lab - 1
+//
+// In this lab, you will learn how to "hook" your add-in to Revit.
+// This lab defines a minimum external command.
+//
+// Explain about add-in manifest, how to create GUID.
+// Hello World in VB.NET is from page 367 of Developer Guide.
+#endregion
+
+namespace IntroCs
+{
+    /// <summary>
+    /// Hello World #1 - A minimal `hello, world` Revit 
+    /// external command with all namespaces fully qualified.
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorld : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("My Dialog Title", "Hello World!");
+
+            return Result.Succeeded;
+        }
+    }
+
+    /// <summary>
+    /// Hello World #2 - simplified without full 
+    /// namespace and using ReadOnly attribute.
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class HelloWorldSimple : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            TaskDialog.Show("My Dialog Title", "Hello World Simple!");
+
+            return Result.Succeeded;
+        }
+    }
+
+    /// <summary>
+    /// Hello World #3 - minimum external application
+    /// Difference: IExternalApplication instead of IExternalCommand.
+    /// In addin manifest, use addin type "Application" and Name instead of Text tag.
+    /// </summary>
+    //public class HelloWorldApp : IExternalApplication
+    //{
+    //    // OnStartup() - called when Revit starts.
+
+    //    public Result OnStartup(UIControlledApplication app)
+    //    {
+    //        TaskDialog.Show("My Dialog Title", "Hello World from App!");
+
+    //        return Result.Succeeded;
+    //    }
+
+    //    // OnShutdown() - called when Revit ends.
+
+    //    public Result OnShutdown(UIControlledApplication app)
+    //    {
+    //        return Result.Succeeded;
+    //    }
+    //}
+
+    /// <summary>
+    /// Command Arguments
+    /// Take a look at the command arguments.
+    /// commandData is the topmost object and
+    /// provides the entry point to the Revit model.
+    /// </summary>
+    [Transaction(TransactionMode.Manual)]
+    public class CommandData : IExternalCommand
+    {
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            // The first argument, commandData, provides access to the top most object model.
+            // You will get the necessary information from commandData.
+            // To see what's in there, print out a few data accessed from commandData
+            //
+            // Exercise: Place a break point at commandData and drill down the data.
+
+            UIApplication uiApp = commandData.Application;
+            Application rvtApp = uiApp.Application;
+            UIDocument uiDoc = uiApp.ActiveUIDocument;
+            Document rvtDoc = uiDoc.Document;
+
+            // Print out a few information that you can get from commandData
+            string versionName = rvtApp.VersionName;
+            string documentTitle = rvtDoc.Title;
+
+            TaskDialog.Show("Revit Intro Lab", "Version Name = " + versionName + "\nDocument Title = " + documentTitle);
+
+            // Print out a list of wall types available in the current rvt project:
+
+            FilteredElementCollector wallTypes  = new FilteredElementCollector(rvtDoc).OfClass(typeof(WallType));
+
+            string s = "";
+            foreach (WallType wallType in wallTypes)
+            {
+                s += wallType.Name + "\r\n";
+            }
+
+            // Show the result:
+
+            TaskDialog.Show("Revit Intro Lab","Wall Types (in main instruction):\n\n" + s);
+
+            // Return failure code to demonstrate output argument handling.
+            // 2nd and 3rd arguments are displayed to the user when the command fails.
+            // 2nd - set a message to display to the user.
+            // 3rd - set elements to highlight on screen.
+
+            return Result.Failed;
+        }
+
+        //#region Revit Macros
+        //public void MyFirstMacro()
+        //{
+        //    TaskDialog.Show("My First Macro", "Hello World!");
+        //}
+
+        //public void MyFirstMacro2()
+        //{
+        //    TaskDialog.Show("My First Macro", "The current model file is ");
+        //    //+ this.Application.ActiveUIDocument.Document.PathName );
+        //}
+        //#endregion // Revit Macros
+    }
+}
---
+++ IntroCs/6_ExtensibleStorage.cs
@@ -0,0 +1,149 @@
+﻿#region Namespaces
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.ExtensibleStorage; // needed for Extensible Storage 
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using Util;
+#endregion
+
+namespace IntroCs
+{
+    [Transaction(TransactionMode.Manual)]
+     class ExtensibleStorage:IExternalCommand
+    {
+        Guid _guid = new Guid("B80DC2A8-323B-4CB9-A50A-8AA95984E4DD");
+
+        class WallSelectionFilter:ISelectionFilter
+        {
+            public bool AllowElement(Element e)
+            {
+                return e is Wall;
+            }
+
+            public bool AllowReference(Reference r, XYZ p)
+            {
+                return true;
+            }
+        }
+
+        public Result Execute(ExternalCommandData commandData, ref string message, ElementSet elements)
+        {
+            UIDocument uiDoc = commandData.Application.ActiveUIDocument;
+            Document doc = uiDoc.Document;
+
+            //создаем транзакцию
+
+            Transaction trans = new Transaction(doc, "Extensible Storage");
+            trans.Start();
+
+            //выбор стены
+
+            Wall wall = null;
+
+            try
+            {
+                Reference r = uiDoc.Selection.PickObject(ObjectType.Element, new WallSelectionFilter());
+                wall = doc.GetElement(r) as Wall;
+            }
+
+            catch (Autodesk.Revit.Exceptions.OperationCanceledException)
+            {
+                message = "Ничего не выбрано; пожалуйста выбирете стену";
+                return Result.Failed;
+            }
+
+            Debug.Assert(null != wall, "стены должны быть выьраны");
+
+            if (null == wall)
+            {
+                message = "пожалуйста выбирете стену";
+                return Result.Failed;
+            }
+
+            SchemaBuilder builder = new SchemaBuilder(_guid);
+
+            builder.SetReadAccessLevel(AccessLevel.Public);
+            builder.SetWriteAccessLevel(AccessLevel.Public);
+
+            builder.SetSchemaName("WallSocketLocation");
+            builder.SetDocumentation("Data stroe for socket relates info in a wall");
+
+            FieldBuilder fieldBuilder1 = builder.AddSimpleField("SocketLocation", typeof(XYZ));
+
+            fieldBuilder1.SetSpec(SpecTypeId.Length);
+
+            FieldBuilder fieldBuilder2 = builder.AddSimpleField("SocketNumber", typeof(string));
+
+            Schema schema = builder.Finish();
+
+            // Create an entity (object) for this schema (class)
+
+            Entity ent = new Entity(schema);
+            Field socketLocation = schema.GetField("SocketLocation");
+            //ent.Set<XYZ>( socketLocation, new XYZ( 2, 0, 0 ), DisplayUnitType.DUT_METERS ); // 2020
+            ent.Set<XYZ>(socketLocation, new XYZ(2, 0, 0), UnitTypeId.Meters); // 2021
+
+            Field socketNumber = schema.GetField("SocketNumber");
+            ent.Set<string>(socketNumber, "200");
+
+            wall.SetEntity(ent);
+
+            // Now create another entity (object) for this schema (class)
+
+            Entity ent2 = new Entity(schema);
+            Field socketNumber1 = schema.GetField("SocketNumber");
+            ent2.Set<String>(socketNumber1, "400");
+            wall.SetEntity(ent2);
+
+            // Note: this will replace the previous entity on the wall 
+
+            // List all schemas in the document
+
+            string s = string.Empty;
+            IList<Schema> schemas = Schema.ListSchemas();
+
+            foreach(Schema sch in schemas)
+            {
+                s += "\r\nSchema Name: " + sch.SchemaName;
+            }
+            TaskDialog.Show("Schema details", s);
+
+            //List all fields for our schema
+
+            s = string.Empty;
+            Schema ourSchema=Schema.Lookup(_guid);
+            IList<Field> fields = ourSchema.ListFields();
+            foreach(Field fld in fields)
+            {
+                s += "\r\nField Name: " + fld.FieldName;
+            }
+            TaskDialog.Show("Field details", s);
+
+            //Extract the value for the field we created
+
+            Entity wallSchemaEnt = wall.GetEntity(Schema.Lookup(_guid));
+
+            XYZ wallSocketPos = wallSchemaEnt.Get<XYZ>(Schema.Lookup(_guid).GetField("SocketLocation"), UnitTypeId.Meters);
+
+            s = "SocketLocation: " + Format.PointString(wallSocketPos);
+
+            string wallSockerNumber = wallSchemaEnt.Get<String>(Schema.Lookup(_guid).GetField("SocketNumber"));
+
+            s += "\r\nSocketNumber: " + wallSockerNumber;
+
+            TaskDialog.Show("Field value", s);
+
+            trans.Commit();
+
+            return Result.Succeeded;
+                
+              
+
+        }
+    }
+}
